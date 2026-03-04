57a2a10dcb2fdc2bde3a806fed80bc20d70f6d77
Alexander <alexandertsyrkin@gmail.com>
2026-03-04 14:06:43 +0200

    fix: load employees from 3001 and normalize birthdate field

--- src/services/ApiClientImpl.ts
+++ src/services/ApiClientImpl.ts
@@ -2,13 +2,21 @@ import axios, { AxiosRequestConfig } from "axios";
 import ApiClient from "./ApiClient";
 import { type Employee } from "../models/Employee";
 import { type EmployeeUpdater } from "../models/EmployeeUpdater";
+
+type ApiEmployee = Employee & {
+    birthDate?: string
+}
+
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/"
+    baseURL: "http://localhost:3001/"
 })
 class ApiClientJsonServer implements ApiClient {
     async getEmployees(config?: AxiosRequestConfig): Promise<Employee[]> {
-        const response = await axiosInstance.get<Employee[]>("employees", config);
-        return response.data
+        const response = await axiosInstance.get<ApiEmployee[]>("employees", config);
+        return response.data.map((employee) => ({
+            ...employee,
+            birthdate: employee.birthdate ?? employee.birthDate ?? "",
+        }))
     }
     addEmployee(_empl: Employee): Promise<Employee> {
         throw new Error("Method not implemented.");
