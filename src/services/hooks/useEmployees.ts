7643e323e706011074bde2e3e02b39056b808731
Alexander <alexandertsyrkin@gmail.com>
2026-03-08 19:46:29 +0200

    feat: add filter params to useEmployees hook

--- src/services/hooks/useEmployees.ts
+++ src/services/hooks/useEmployees.ts
@@ -1,16 +1,21 @@
-import { AxiosError, AxiosRequestConfig } from "axios";
+import { AxiosError } from "axios";
 import { Employee } from "../../models/Employee";
 import { useQuery } from "@tanstack/react-query";
 import apiClient from "../ApiClientImpl";
+import { FilterFields } from "../../models/FilterFields";
 
-export default function useEmployees(config?: AxiosRequestConfig): {employees: Employee[], isLoading: boolean,
-     error: AxiosError | null} {
-        const queryKey: any[] = ["employees"];
-        config && queryKey.push(config)
-        const result = useQuery<Employee[], AxiosError>({
-            queryKey,
-            queryFn: () => apiClient.getEmployees(config),
-            staleTime: 3600_000
-        })
-        return {employees: result.data || [], error: result.error, isLoading: result.isLoading}
-     }
+export default function useEmployees(filters?: FilterFields): {
+    employees: Employee[],
+    isLoading: boolean,
+    error: AxiosError | null
+} {
+    const queryKey: any[] = ["employees", filters];
+
+    const result = useQuery<Employee[], AxiosError>({
+        queryKey,
+        queryFn: () => apiClient.getEmployees(filters),
+        staleTime: 3600_000
+    });
+
+    return { employees: result.data || [], error: result.error, isLoading: result.isLoading };
+}
